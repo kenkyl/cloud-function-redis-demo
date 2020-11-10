7bdaaeeb860919155f66a04ded23c8d02d983136
Kyle Kennedy <kylekennedy@Kyles-MBP.attlocal.net>
2020-11-10 15:36:09 -0600

    little changes. linking repo to cloud function

--- src/helloWorld.js
+++ src/helloWorld.js
@@ -5,7 +5,6 @@
  * @param {!express:Response} res HTTP response context.
  */
 
-var async = require('async');
 var redis = require('redis');
 
 var redisHost = process.env.REDIS_HOST;
@@ -17,7 +16,7 @@ var redisClient = redis.createClient({host: redisHost, port: redisPort, password
 const STREAMS_KEY = "cloudsql-stream";
 
 exports.helloWorld = (req, res) => {
-    let value = null
+    var value = null
   
     redisClient.ping(function(err, reply) {
         if (err) throw err;
